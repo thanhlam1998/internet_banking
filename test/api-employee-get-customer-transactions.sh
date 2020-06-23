b963e317b28893aa4d74036189afc0a96c4b21a9
Khue Doan <khuedoan98@gmail.com>
2020-06-23 23:38:26 +0700

    Update get customer transactions test

--- test/api-employee-get-customer-transactions.sh
+++ test/api-employee-get-customer-transactions.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 curl -X GET \
-  -H 'access_token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbXBsb3llZV9pZCI6MSwiaWF0IjoxNTkyNzUyMzQwLCJleHAiOjE1OTI3NTgzNDB9.Ui2Pzc5kZJCx1fF1bbY9TL4dtTlAc0ouuJyCvJVl91k' \
-  'http://localhost/api/employee/get-customer-transactions?customer_id=3'
+  -H 'access_token: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbXBsb3llZV9pZCI6MSwiaWF0IjoxNTkyOTMwMjIyLCJleHAiOjE1OTI5MzYyMjJ9.4xCMkdz56hMmX5U-Z7Y2SNm5wVfmISNeaPctSzm2Vk0' \
+  'http://localhost/api/employee/get-customer-transactions?credit_number=025917154505'
