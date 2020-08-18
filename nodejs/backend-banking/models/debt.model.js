419a2df6072635c1c3dffddc25a554f0851be372
Khue Doan <khuedoan98@gmail.com>
2020-08-19 03:42:52 +0700

    Fix bedebt missing credit_number

--- nodejs/backend-banking/models/debt.model.js
+++ nodejs/backend-banking/models/debt.model.js
@@ -16,5 +16,5 @@ module.exports = {
   },
   deactive_debt: debt_id => db.load(`update debt set status=false where debt_id=${debt_id}`),
   get_list_debt: customer_id => db.load(`SELECT debt.*, credit_account.credit_number FROM debt INNER JOIN credit_account ON debt.lender_id=credit_account.customer_id WHERE debt.lender_id=${customer_id} AND debt.status=1`),
-  get_list_bedebt: customer_id => db.load(`select * from debt where debtor_id=${customer_id} and status=1`)
+  get_list_bedebt: customer_id => db.load(`SELECT debt.*, credit_account.credit_number FROM debt INNER JOIN credit_account ON debt.lender_id=credit_account.customer_id WHERE debt.debtor_id=${customer_id} AND debt.status=1`)
 };
